9e4fb70faea5d9cb12dcb4803e0dcbf85ee6edc8
matias tajan <matias.tajan@gmail.com>
2025-08-09 20:50:50 -0400

    Made some changes

---
+++ Prototype/Data/nearby_countries.py
@@ -0,0 +1,136 @@
+venezuela_neighboring_countries = [
+    "colombia",
+    "brazil",
+    "guyana"
+]
+united_states_neighboring_countries = [
+    "canada",
+    "mexico"
+]
+chile_neighboring_countries = [
+    "argentina",
+    "bolivia",
+    "peru"
+]
+germany_neighboring_countries = [
+    "denmark",
+    "poland",
+    "czech republic",
+    "austria",
+    "switzerland",
+    "france",
+    "luxembourg",
+    "belgium",
+    "netherlands"
+]
+france_neighboring_countries = [
+    "belgium",
+    "luxembourg",
+    "germany",
+    "switzerland",
+    "italy",
+    "monaco",
+    "spain",
+    "andorra",
+]
+italy_neighboring_countries = [
+    "austria",
+    "france",
+    "holy see",
+    "san marino",
+    "slovenia",
+    "switzerland"
+]
+united_kingdom_neighboring_countries = [
+    "republic of ireland"
+]
+china_neighboring_countries = [
+    "north korea",
+    "russia",
+    "mongolia",
+    "kazakhstan",
+    "kyrgyzstan",
+    "tajikistan",
+    "afghanistan",
+    "pakistan",
+    "india",
+    "nepal",
+    "bhutan",
+    "myanmar",
+    "laos",
+    "vienam"
+
+]
+south_korea_neighboring_countries = [
+    "north korea"
+]
+canada_neighboring_countries = [
+    "united states"
+]
+russia_neighboring_countries = [
+    "poland",
+    "lithuania",
+    "norway",
+    "finland",
+    "estonia",
+    "latvia",
+    "belarus",
+    "ukraine",
+    "georgia",
+    "azerbaijan"
+    "kazakhstan",
+    "china",
+    "mongolia",
+    "north korea"
+]
+israel_neighboring_countries = [
+    "lebanon",
+    "syria",
+    "jordan",
+    "egypt"
+]
+brazil_neighboring_countries = [
+    "argentina",
+    "uruguay",
+    "paraguay",
+    "peru",
+    "bolivia",
+    "colombia",
+    "venezuela",
+    "suriname",
+    "guyana",
+    "french guyana"
+]
+india_neighboring_countries = [
+    "afghanistan",
+    "bangladesh",
+    "bhutan",
+    "china",
+    "maldives",
+    "myanmar",
+    "nepal",
+    "pakistan",
+    "sri lanka"
+]
+spain_neighboring_countries = [
+    "morocco",
+    "andorra",
+    "france",
+    "portugal",
+    "gibraltar"
+]
+honduras_neighboring_countries = [
+    "guatemala",
+    "nicaragua",
+    "el salvador"
+]
+north_korea_neighboring_countries = [
+    "china",
+    "russia",
+    "south korea"
+]
+morocco_neighboring_countries = [
+    "spain",
+    "western sahara",
+    "algeria"
+]
